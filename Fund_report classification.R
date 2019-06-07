dd98110e92b420761fd71b6cbb75ce6446cecf9a
paiyating <50113220+paiyating@users.noreply.github.com>
2019-06-07 12:28:12 +0800

    Add files via upload

--- Fund_report classification.R
+++ Fund_report classification.R
@@ -4,60 +4,74 @@ library("readxl")
 test <- readxl::read_excel("201905.xlsx")
 test$權重=round(test$權重,4)
 
-for (i in c(1:ncol(test))) {
+for (i in c(7:ncol(test))) {
   for (j in c(1:nrow(test))) {
     if(grepl("N/A",test[j,i])){
       test[j,i]="N/A";
     }
   }
 }
-
-final<-cbind(test$基金名稱,test$月底規模)
-colnames(final)<-c("基金名稱","規模") 
-temp<-cbind(test$基金名稱,test$權重)
-temp=aggregate(as.numeric(temp[,2]), by=list(temp[,1]), FUN=sum)
-
-for (i in c(1:nrow(temp))) {
-  if(0<temp[i,2]&&temp[i,2]<1&&(!is.na(temp[i,2]))){
-    temp[i,2]=round(1-temp[i,2],4);
+for (i in c(1:nrow(test))) {
+  if(is.na(test$權重[i])){
+    test$權重[i]=0;
   }
 }
-colnames(temp)<-c("基金名稱","現金")
-final=merge(final,temp,by="基金名稱")
+final<-cbind(test$基金名稱,test$月底規模)
+colnames(final)<-c("基金名稱","規模") 
 final=unique(final)
+temp<-cbind(test$基金名稱,test$權重)
+holding=aggregate(as.numeric(temp[,2]), by=list(temp[,1]), FUN=sum)
+colnames(holding)<-c("基金名稱","持股")
+holding=mutate(as.data.frame(holding),"現金"=round(1-as.numeric(holding[,2]),4))
+for (i in c(1:nrow(holding))) {
+  if(((0>holding$持股[i])|(holding$持股[i]>1))&&(!is.na(holding$持股[i]))){
+    holding$現金[i]=0;
+  }
+}
+final=merge(final,holding,by="基金名稱")
+write.csv(final, file = "現金&持股比率.csv", row.names = FALSE)
+holding<-NULL
+final<-NULL
+temp<-NULL
 
 mer=cbind(test$基金名稱,test[,7],test$權重)
 mer=aggregate(as.numeric(mer[,3]), by=list(mer[,1],mer[,2]), FUN=sum)
 colnames(mer)<-c("基金名稱","國家","比率")
-final=merge(final,mer,all = TRUE)
-final=do.call(rbind, lapply(split(final, final$基金名稱), function(x) x[order(x$比率,decreasing = TRUE),]))
-write.csv(final, file = "現金&國家.csv", row.names = FALSE)
+mer=do.call(rbind, lapply(split(mer, mer$基金名稱), function(x) x[order(x$比率,decreasing = TRUE),]))
+write.csv(mer, file = "國家.csv", row.names = FALSE)
+mer<-NULL
 
 mer=cbind(test$基金名稱,test[,8],test$權重)
 mer=aggregate(as.numeric(mer[,3]), by=list(mer[,1],mer[,2]), FUN=sum)
 colnames(mer)<-c("基金名稱","產業","比率")
-mydf <- as.data.frame(mer)
-mydf=do.call(rbind, lapply(split(mydf, mydf$基金名稱), function(x) x[order(x$比率,decreasing = TRUE)[],]))
-write.csv(mydf, file = "產業.csv", row.names = FALSE)
+mer <- as.data.frame(mer)
+mer=do.call(rbind, lapply(split(mer, mer$基金名稱), function(x) x[order(x$比率,decreasing = TRUE)[],]))
+write.csv(mer, file = "產業.csv", row.names = FALSE)
+mer<-NULL
 
 mer=cbind(test$基金名稱,test$股票名稱,test[,16],test$`GICS Sector Name`,test$`GICS Sub-Industry Name`,test$權重)
 colnames(mer)<-c("基金名稱","公司","國家","產業","次產業","比率")
-mydf <- as.data.frame(mer)
-mydf=do.call(rbind, lapply(split(mydf, mydf$基金名稱), function(x) x[order(x$比率,decreasing = TRUE)[1:10],]))
-write.csv(mydf, file = "十大持股.csv", row.names = FALSE)
+mer <- as.data.frame(mer)
+mer=do.call(rbind, lapply(split(mer, mer$基金名稱), function(x) x[order(x$比率,decreasing = TRUE)[1:10],]))
+mer=mer[complete.cases(mer[,1]),]
+write.csv(mer, file = "十大持股.csv", row.names = FALSE)
+mer<-NULL
 
 for (i in c(10,11,15)) {
-  temp<-cbind(test$基金名稱,test[,i],test$權重)
-  temp=aggregate(as.numeric(temp[,3]), by=list(temp[,1],temp[,2]), FUN=sum)
-  colnames(temp)<-c("基金名稱",colnames(test[i]),"比率")
-  mydf <- as.data.frame(temp)
+  mer<-cbind(test$基金名稱,test[,i],test$權重)
+  mer=aggregate(as.numeric(mer[,3]), by=list(mer[,1],mer[,2]), FUN=sum)
+  colnames(mer)<-c("基金名稱",colnames(test[i]),"比率")
+  mer <- as.data.frame(mer)
   if(i==10)
-    mydf=do.call(rbind, lapply(split(mydf, mydf$基金名稱), function(x) x[order(x[,"S&P Rating"],decreasing = FALSE)[],]))
+    mer=do.call(rbind, lapply(split(mer, mer$基金名稱), function(x) x[order(x[,"S&P Rating"],decreasing = FALSE)[],]))
   else
-    mydf=do.call(rbind, lapply(split(mydf, mydf$基金名稱), function(x) x[order(x$比率,decreasing = TRUE)[],]))
-  write.csv(mydf, file = paste0(names(test[i]), '.csv'), row.names = FALSE)
+    mer=do.call(rbind, lapply(split(mer, mer$基金名稱), function(x) x[order(x$比率,decreasing = TRUE)[],]))
+  write.csv(mer, file = paste0(names(test[i]), '.csv'), row.names = FALSE)
 }
-
+mer<-NULL
+test<-NULL
+i<-NULL
+j<-NULL
 # mer=cbind(test$基金名稱,test[,8],test[,9],test$權重)
 # mer=aggregate(as.numeric(mer[,4]), by=list(mer[,1],mer[,2],mer[,3]), FUN=sum)
 # colnames(mer)<-c("基金名稱","產業","次產業","比率")
